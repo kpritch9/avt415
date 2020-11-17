7b6a5c038bb8424b26b7b17470d682af36894d78
Kathryn Pritchard <pritchard_kathryn@bah.com>
2020-11-16 22:42:26 -0500

    a4 - history

--- assignment4_js/index.js
+++ assignment4_js/index.js
@@ -45,14 +45,34 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      isXNext: true
+      isXNext: true,
+	  stepNumber: 0;
     };
+  }
+  
+  jumpTo(step) {
+	this.setState({
+		stepNumber: step,
+		isXNext: (step % 2) === 0,
+	});
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+	
+	const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+	
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -62,12 +82,13 @@ class Game extends React.Component {
         squares: squares
       }]),
       isXNext: !this.state.isXNext,
+	  stepNumber: history.length,
     });
   }
 	
   render() {
 	  const history = this.state.history;
-	  const current = history[history.length - 1];
+	  const current = history[this.state.stepNumber];
 	  const winner = calculateWinner(current.squares);
 	  let status;
 	  if(winner) {
@@ -85,7 +106,7 @@ class Game extends React.Component {
 		</div>
 		<div className="game-info">
 		  <div>{status}</div>
-		  <ol>{/* TODO */}</ol>
+		  <ol>{moves}</ol>
 		</div>
 	  </div>
 	);
