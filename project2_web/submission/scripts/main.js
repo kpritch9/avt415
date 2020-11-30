49800ef2252b83fa47a23f987d519403b92723b1
Kathryn Pritchard <pritchard_kathryn@bah.com>
2020-11-29 20:00:34 -0500

    p2 - sign up email popup

--- project2_web/submission/scripts/main.js
+++ project2_web/submission/scripts/main.js
@@ -1,63 +1,24 @@
-	$(document).ready(function () {		
-		$('.nav-icon').on('click', toggleNav);
-		$('#cancel').on('click', toggleNav);
+	$(document).ready(function () {	
+		var popup = $('#popup');
+	
+		$('#emailSignup').on('click', togglePopup);
+		$('.close').on('click', togglePopup);
 		
+		window.onclick = function(event) {
+		  if (event.target == popup) {
+			togglePopup();
+			}
+		}
 
 	});
 	
-	    /*$(window).scroll(function() {    
-        
-		var header = $("#navWrapper");
-		var scroll = $(window).scrollTop();
-		var winHeight = $(window).height();
-    
-        if (scroll >= (winHeight - 200)) {
-            header.addClass('navShadow');
-        } else {
-            header.removeClass('navShadow');
-        }
-    });*/
-	
-	function toggleNav() {	
-			if($('#links').hasClass('toggle-on')){
-				$('#links').removeClass('toggle-on');
-				leadOutLinks();
-				$('.hamburger-line-1').css('transform', 'translateY(0px) rotate(0deg)');
-				$('.hamburger-line-2').css('transform', 'scale(1)');
-				$('.hamburger-line-3').css('transform', 'translateY(0px) rotate(0deg)');
-			}
-			else {
-				
-				$('#links').addClass('toggle-on');
-				leadInLinks();
-				$('.hamburger-line-1').css('transform', 'translateY(8px) rotate(135deg)');
-				$('.hamburger-line-2').css('transform', 'scale(0)');
-				$('.hamburger-line-3').css('transform', 'translateY(-8px) rotate(-135deg)');
-				
-			}
-	}
-
-	function leadInLinks() {
-		console.log("lead in");
-		anime({
-			targets: '#links .nav-link',
-			translateY: 100,
-			delay: function(el, i, l) {
-				return i * 100;
-			}
-						
-		});
-	}
-	
-	function leadOutLinks() {
-		console.log("lead in");
-		anime({
-			targets: '#links .nav-link',
-			translateY: -100,
-			delay: function(el, i, l) {
-				return i * 100;
-			}
-						
-		});
+	function togglePopup() {	
+	console.log("toggle");
+		var popup = $('#popup');
+		if (popup.hasClass('hide')) {
+			popup.removeClass('hide');
+		} else {
+			popup.addClass('hide');
+		}
 	}
 	
